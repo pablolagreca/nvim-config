22632fac65589b3f819a70b498cce73bd9d7763c
PLG <pablolagreca@gmail.com>
2023-01-22 23:20:43 -0300

    fixing init.lua

--- init.lua
+++ init.lua
@@ -1,41 +1,3 @@
-<<<<<<< HEAD
-require('plugins')
-require('lightline')
-require('config')
-require('lightline')
-vim.opt.relativenumber=true
-vim.opt.autoindent=true
-vim.opt.tabstop=4
-vim.opt.shiftwidth=4
-vim.opt.smarttab=true
-vim.opt.softtabstop=4
--- vim.opt.mouse=a
-vim.opt.undofile=true
-vim.opt.timeoutlen=500
-vim.opt.encoding='UTF-8'
-vim.opt.termguicolors=true
-
--- vim.opt.completeopt-=preview  --For No Previews
-
-
-vim.cmd(':colorscheme jellybeans')
-
--- air-line
-vim.g.airline_powerline_fonts = 1
-
-if vim.g.airline_symbols == nil then
-    vim.g.airline_symbols = {}
-end
-
--- airline symbols
-vim.g.airline_left_sep = ''
-vim.g.airline_left_alt_sep = ''
-vim.g.airline_right_sep = ''
-vim.g.airline_right_alt_sep = ''
-vim.g.airline_symbols.branch = ''
-vim.g.airline_symbols.readonly = ''
-vim.g.airline_symbols.linenr = ''
-=======
 local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
 local is_bootstrap = false
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
@@ -633,4 +595,3 @@ end
 
 M.setup()
 
->>>>>>> b36b909 (adding cutomized configuration of kick-start)
